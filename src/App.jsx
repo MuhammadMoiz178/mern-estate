47e48d0d174843c10b0b305351cb8d16ff6a0651
Muhammad Moiz <moizmuhammad467@gmail.com>
2025-11-25 23:14:28 +0500

    creaate header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 function App() {
   return (
     <BrowserRouter>
+    {/* Header is in all pages */}
+    <Header/>                              
     <Routes>
       <Route path='/' element={<Home/>}/> 
       <Route path='/sign-in' element={<Signin/>}/> 

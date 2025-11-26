5ab0d5579f87a5aa27ab60cd85fdd9edd1ee5300
Muhammad Moiz <moizmuhammad467@gmail.com>
2025-11-26 23:36:43 +0500

    Connect to Database

---
+++ api/connectDb.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+
+const connectDb=async ()=>{
+    try {
+        dotenv.config();
+        console.log(process.env.MONGODB_URI)
+    const connectionInstance=await mongoose.connect(process.env.MONGODB_URI)
+    console.log(`MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`);
+    
+    } catch (error) {
+        console.log("MongoDb error",error);
+        process.exit(1)
+    }
+}
+
+export default connectDb
--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import connectDb from "./connectDb.js"
 
+dotenv.config({
+    path:"./.env"
+})
 const app = express()
+
+connectDb()
 
 app.listen(3000,()=>console.log("Server is running on port 3000"))

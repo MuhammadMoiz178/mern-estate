7c81f3502575587c305a26478163a5d1cfc59940
Muhammad Moiz <moizmuhammad467@gmail.com>
2025-11-27 19:05:43 +0500

    create signup api route

--- api/connectDb.js
+++ api/connectDb.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 const connectDb=async ()=>{
     try {
         dotenv.config();
-        console.log(process.env.MONGODB_URI)
+        // console.log(process.env.MONGODB_URI)
     const connectionInstance=await mongoose.connect(process.env.MONGODB_URI)
     console.log(`MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`);
     
--- api/index.js
+++ api/index.js
@@ -2,17 +2,18 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./connectDb.js"
 import userRouter from "./routes/user.route.js"
-
+import authRouter from "./routes/auth.route.js"
 dotenv.config({
     path:"./.env"
 })
 
 connectDb()
-
-
 const app = express()
+
+app.use(express.json())
+
 app.use("/api/user",userRouter)
+app.use("/api/auth",authRouter)
 
-
-app.listen(3000,()=>console.log("Server is running on port 3000"))
+app.listen(8000,()=>console.log("Server is running on port 3009"))
 

4717caa501b71e4ab2a4c7cb4f36b7f8ed26cb09
Muhammad Moiz <moizmuhammad467@gmail.com>
2025-11-28 17:52:53 +0500

    Create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import {User} from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
-
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
     // console.log(req.body)
@@ -18,7 +19,28 @@ export const signup = async (req, res, next) => {
     await user.save();
     res.status(201).json("User created successfully");
   } catch (error) {
-    console.log("SIGNUP ERROR:", error);   // <<<<<< ADD THIS
+    console.log("SIGNUP ERROR:", error);   
     next(error);
   }
 };
+
+
+
+export const signin = async (req,res,next) => {
+      const {email,password} = req.body
+      try {
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandler(404,'User not found'))
+         const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword) return next(errorHandler(401,'Wrong credentials')) 
+        //means user email and pass is correct and we are gonna generate token
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const newUser = await User.findById(validUser._id).select("-password")
+        res
+        .cookie('access_token',token,{httpOnly:true})
+        .status(200)
+        .json(newUser)  
+      } catch (error) {
+        next(error)
+      }
+}

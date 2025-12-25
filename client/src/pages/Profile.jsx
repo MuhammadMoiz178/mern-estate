00a26398a72444a42e3d117f169d20ce40040c60
Muhammad Moiz <moizmuhammad467@gmail.com>
2025-12-25 20:39:16 +0500

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -205,7 +205,9 @@ function Profile() {
                 >
                   Delete
                 </button>
+                <Link to={`/update-listing/${listing._id}`}>
                   <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}

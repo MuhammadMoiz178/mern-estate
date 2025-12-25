6ac01e0075b5ba45349fb3e69b82acf4b9eb8797
Muhammad Moiz <moizmuhammad467@gmail.com>
2025-12-25 18:07:57 +0500

    complete delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -124,6 +124,22 @@ function Profile() {
       showListingsError(true)
     }
   }
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+      })
+      const data = await res.json()
+      if(data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListing((prev)=>prev.filter((listing)=>listing._id !== listingId))
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className=' max-w-lg mx-auto p-3'>
       <h1 className='text-3xl text-center my-7'>Profile</h1>
@@ -184,7 +200,7 @@ function Profile() {
               </Link>
 
               <div className='flex flex-col item-center'>
-                <button
+                <button onClick={()=>handleListingDelete(listing._id)}
                   className='text-red-700 uppercase'
                 >
                   Delete
